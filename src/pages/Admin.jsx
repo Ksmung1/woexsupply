5565355f31d6982801fd01f6a45a5cc24e0817ae
Milestone Paite <crepybozz@gmail.com>
2025-12-13 20:04:18 +0530

    git psh

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -29,6 +29,23 @@ useEffect(() => {
 }, []);
 
 
+const checkApi = async () => {
+  const payload ={
+    "customer_mobile": "8877332212",
+    "user_token": "16b5fc1d365c1a173f2119037af3be55",
+    "amount": "1",
+    "order_id": "8787772321800",
+    "redirect_url": "https://localhost:5173/",
+    "remark1" : "testremark",
+    "remark2": "testremark2"
+    }
+
+    const res = await axios.post(`https://paymentgateway.unaux.com/api/create-order`, payload)
+    console.log(res.data)
+    return 
+}
+
+
   return (
     <div>
         <section className='flex gap-2 max-w-7xl mt-20 mx-auto px-4 md:px-6 lg:px-8'>
@@ -39,6 +56,7 @@ useEffect(() => {
           <div className=' flex gap-2 p-1 border rounded border-gray-200'>
             <h1 className='font-semibold'>GTU Balance:</h1>
             <p>{gtBalance}</p>
+            <p onClick={()=>checkApi()}>CHECK API</p>
           </div>
           </section>
     </div>
